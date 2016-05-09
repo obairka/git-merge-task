d318100a7f66fef9be6a07d843abae59e035b463
Baira Oydopova <obairka@gmail.com>
2016-05-09 11:57:54 +0500

    Add twoargs handling

--- Calculator.cs
+++ Calculator.cs
@@ -18,7 +18,8 @@ namespace Kontur.Courses.Git
 				//> 4
 				// + 1
 				//>5
-				return lastResult = Maybe<double>.FromError("Not implemented yet");
+				var v2 = double.Parse(args[1]);
+				return lastResult = Execute(args[0], lastResult.Value, v2);
 			}
 			if (args.Length == 3)
 			{
